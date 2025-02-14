33c3530df1bd337545a2d65f7d92b3dffce64cf7
Lucas Morais de Oliveira <lucasmoliveira059@gmail.com>
2025-02-13 22:50:31 -0300

    Adicionando função webvulnerability

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description="Uma biblioteca Python para interagir com o Metasploit, Nmap e Wireshark.",
     author="Seu Nome",
     packages=find_packages(),
-    install_requires=["pymetasploit3", "python-nmap", "pyshark"],
+    install_requires=["pymetasploit3", "python-nmap", "pyshark", "googlesearch-python", "requests"],
     python_requires=">=3.6",
 )
